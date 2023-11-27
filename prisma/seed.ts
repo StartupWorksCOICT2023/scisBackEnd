25811491bda6ed96ce99fbea786c2cbeeaa93f78
midnightCODA <mbunjimo@gmail.com>
2023-11-27 18:11:41 +0300

    seeding data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,25 +3,46 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function seed() {
-  // Create roles
+  // Create roles if they don't exist
   const roles = ['teacher', 'admin', 'student', 'superadmin', 'parent'];
 
   for (const roleName of roles) {
-    await prisma.role.create({
-      data: {
+    const existingRole = await prisma.role.findUnique({
+      where: {
         name: roleName,
       },
     });
+
+    if (!existingRole) {
+      await prisma.role.create({
+        data: {
+          name: roleName,
+        },
+      });
+    }
   }
 
-  // Create a user
-  const adminUser = await prisma.scisUser.create({
-    data: {
-      password: 'yourpasswordhere',
+  // Check if the user with scisuserid '2020-00-00000' already exists
+  const existingUser = await prisma.scisUser.findUnique({
+    where: {
       scisuserid: '2020-00-00000',
-      roleId: 'admin',
     },
   });
+
+  // Create a user if not present
+  if (!existingUser) {
+    const adminUser = await prisma.scisUser.create({
+      data: {
+        password: 'yourpasswordhere',
+        scisuserid: '2020-00-00000',
+        roleId: 'admin',
+      },
+    });
+
+    console.log('User created successfully!');
+  } else {
+    console.log('User already exists. Skipping user creation.');
+  }
 
   console.log('Seed data created successfully!');
 }
